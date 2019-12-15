c71dfc610edc3948bf5ca7b9bbe6ec2842e3eb82
alexknipfer <alexanderknipfer@gmail.com>
2019-12-15 18:12:52 -0500

    Setup project and todo model

---
+++ CoreDataTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataTodo
+//
+//  Created by Alexander Knipfer on 12/15/19.
+//  Copyright © 2019 Alexander Knipfer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataTodo/TodoItem.swift
@@ -0,0 +1,28 @@
+//
+//  TodoItem.swift
+//  CoreDataTodo
+//
+//  Created by Alexander Knipfer on 12/15/19.
+//  Copyright © 2019 Alexander Knipfer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class TodoItem: NSManagedObject, Identifiable {
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var title: String?
+}
+
+extension TodoItem {
+    static func getAllTodoItems() -> NSFetchRequest<TodoItem> {
+        let request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest() as!
+            NSFetchRequest<TodoItem>
+        
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
